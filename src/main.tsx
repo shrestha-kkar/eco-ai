974cc755346050a77998c58008c594f02854692e
Shrestha Thakkar <116107599+shrestha-kkar@users.noreply.github.com>
2026-07-21 11:58:22 +0530

    Replace site with temporary under-construction page
    Full site is preserved on the pre-launch-backup branch for later restoration.

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
-import Layout from './components/Layout.tsx'
-import Home from './App.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
